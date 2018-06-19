0ba724031d9858bd258105d6427641849d11ce3d
Shirley Kiang <kiang.shirley@gmail.com>
2018-06-18 18:41:37 -0700

    commit server test

---
+++ test/server.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const app = require("../server");
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe("Basic Express setup", () => {
+    describe("Express static", () => {
+
+        it('GET request "/" should return the index page', () => {
+            return chai.request(app)
+            .get("/")
+            .then(function (res) {
+                expect(res).to.exist;
+                expect(res).to.have.status(200);
+                expect(res).to.be.html;
+            });
+        });
+    });
+})
